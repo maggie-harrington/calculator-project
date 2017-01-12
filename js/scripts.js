6b87ea28adbb9a77a5ac986844ee31782a7877b6
Maggie Harrington <maggie.harrington@gmail.com>
2017-01-11 17:43:52 -0800

    add user interface with forms and buttons replacing prompt dialog boxes

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,14 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+$(document).ready(function() {
 
-alert (add(number1, number2));
-alert (subtract(number1, number2));
-alert (multiply(number1, number2));
-alert (divide(number1, number2));
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+
+  alert (add(number1, number2));
+//  alert (subtract(number1, number2));
+//  alert (multiply(number1, number2));
+//  alert (divide(number1, number2));
+
+});
