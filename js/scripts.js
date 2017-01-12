18dc6831f8cec1983802567d3aa2a62b4d690eb4
Maggie Harrington <maggie.harrington@gmail.com>
2017-01-11 18:12:20 -0800

    change output to display on page, replacing alert. addition portion is functional

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,15 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-
-  alert (add(number1, number2));
-//  alert (subtract(number1, number2));
-//  alert (multiply(number1, number2));
-//  alert (divide(number1, number2));
-
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1,number2);
+    $("#output").text(result);
+  //  alert (add(number1, number2));
+  //  alert (subtract(number1, number2));
+  //  alert (multiply(number1, number2));
+  //  alert (divide(number1, number2));
+  });
 });
